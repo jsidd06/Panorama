0de3fc4548ddcd56be1e3f247e99e8fccc4c14dc
Siddharth Jain <jsidd06gmail.com@Siddharths-MacBook-Air.local>
2024-04-15 17:32:26 +0530

    feat: demo api test

--- src/container/shareContainer/weather/weather.tsx
+++ src/container/shareContainer/weather/weather.tsx
@@ -1,8 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {HeaderComp} from '@/components';
+import axios from 'axios';
 
 const WeatherScreen = () => {
+  useEffect(() => {
+    axios
+      .get('https://api.api-ninjas.com/v1/weather?city=London', {
+        headers: {
+          'X-Api-Key': '9Dbd2Jht3NoqZMIi1ls9SA==FqzvbtzFotW8lmhZ',
+        },
+      })
+      .then(res => console.log('res', res))
+      .catch(err => console.log('error', err.response));
+  }, []);
   return (
     <View>
       <HeaderComp title="Weather" />
