0ea293c59cef86d69fad7ef762da5d9af84bfbb8
Siddharth Jain <jsidd06gmail.com@Siddharths-MacBook-Air.local>
2024-04-16 20:10:45 +0530

    feat: api cofig set

--- src/services/apis/apis.ts
+++ src/services/apis/apis.ts
@@ -1,15 +1,8 @@
-import axios from 'axios';
 import Axios from '../base/Axios';
 
 export const fetchWeatherData = async (search: string) => {
   try {
-    const response = await Axios.get(`weather?city=${search}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'X-Api-Key': '9Dbd2Jht3NoqZMIi1ls9SA==FqzvbtzFotW8lmhZ',
-      },
-    });
+    const response = await Axios.get(`weather?city=${search}`);
     return response.data;
   } catch (err: any) {
     throw err.response.data.error;
