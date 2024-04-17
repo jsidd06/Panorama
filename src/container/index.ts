0a9062fc2be85bd02fd040b1dff2713683a02d24
Siddharth Jain <jsidd06gmail.com@Siddharths-MacBook-Air.local>
2024-04-17 21:01:48 +0530

    feat: add screen folder or images size compressed

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,4 +1,4 @@
 export {default as HomeScreen} from '@/container/homeContainer/screens/index';
-export {default as WeatherScreen} from '@/container/shareContainer/weather/weather';
+export {default as WeatherScreen} from '@/container/shareContainer/screens/weather/weather';
 export {default as OnBoardingScreen} from '@/container/onBoardingContainer/screens/index';
-export {default as DogsScreen} from '@/container/shareContainer/dogs/dogs';
+export {default as DogsScreen} from '@/container/shareContainer/screens/dogs/dogs';
---
+++ src/container/shareContainer/screens/babyNames/babyNames.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const BabyNamesScreen = () => {
+  return (
+    <View>
+      <Text>BabyNamesScreen</Text>
+    </View>
+  );
+};
+
+export default BabyNamesScreen;
+
+const styles = StyleSheet.create({});
