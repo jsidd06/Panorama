1a5c8a23d9339c3320c18dc62528aad0f6ea1385
Siddharth Jain <jsidd06gmail.com@Siddharths-MacBook-Air.local>
2024-04-22 07:26:13 +0530

    feat: download image functionality added

--- src/container/shareContainer/screens/logos/logo.tsx
+++ src/container/shareContainer/screens/logos/logo.tsx
@@ -1,5 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Alert,
+  Image,
+  Platform,
+  PermissionsAndroid,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Pressable,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {ErrorComp, HeaderComp, LoadingComp, SearchComp} from '@/components';
 import {useDispatch, useSelector} from 'react-redux';
@@ -7,6 +18,7 @@ import {COLORS} from '@/themes/Colors';
 import {fetchLogosData} from '@/services/apis/apis';
 import {setLogosData} from '@/redux/featuresSlice/allDataSlice';
 import {FontSize, Layout, MetricsSizes} from '@/themes/style';
+import RNFetchBlob from 'rn-fetch-blob';
 
 const LogosScreen = () => {
   const dispatch = useDispatch();
@@ -23,13 +35,68 @@ const LogosScreen = () => {
     setLoading(true);
     try {
       const data = await fetchLogosData(search);
-      console.log('data', data);
       setLoading(false);
       dispatch(setLogosData(data));
     } catch (err: any) {
       setLoading(false);
       setError(err);
     }
+  };
+
+  const checkPermission = async (image: any) => {
+    if (Platform.OS === 'ios') {
+      downloadImage();
+    } else {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Storage Permission Required',
+            message: 'App needs access to your storage to download Photos',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Storage Permission Granted.');
+          downloadImage(image);
+        } else {
+          Alert.alert('Storage Permission Not Granted');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+  };
+
+  const downloadImage = (image: any) => {
+    let date = new Date();
+    let image_URL = image;
+    let ext: any = getExtention(image_URL);
+    ext = '.' + ext[0];
+    const {config, fs} = RNFetchBlob;
+    let PictureDir = fs.dirs.PictureDir;
+    let options = {
+      fileCache: true,
+      addAndroidDownloads: {
+        useDownloadManager: true,
+        notification: true,
+        path:
+          PictureDir +
+          '/image_' +
+          Math.floor(date.getTime() + date.getSeconds() / 2) +
+          ext,
+        description: 'Image',
+      },
+    };
+    config(options)
+      .fetch('GET', image_URL)
+      .then(res => {
+        console.log('res -> ', JSON.stringify(res));
+        Alert.alert('Image Downloaded Successfully.');
+      });
+  };
+
+  const getExtention = filename => {
+    return /[.]/.exec(filename) ? /[^.]+$/.exec(filename) : undefined;
   };
 
   return loading ? (
@@ -48,7 +115,7 @@ const LogosScreen = () => {
         />
         <ScrollView>
           <View style={[styles.container]}>
-            {store.length > 0 ? (
+            {store?.length > 0 ? (
               store?.map((d: any, i: number) => (
                 <View key={i} style={styles.subCont}>
                   <View style={[Layout.alignCenter]}>
@@ -62,6 +129,12 @@ const LogosScreen = () => {
                     <View style={[Layout.rowACenter]}>
                       <Text style={styles.heading}>Ticker:-</Text>
                       <Text style={styles.subHeading}> {d.ticker}</Text>
+                    </View>
+                    <View style={[Layout.rowACenter]}>
+                      <Text style={styles.heading}>Download logo:-</Text>
+                      <Pressable onPress={() => checkPermission(d.image)}>
+                        <Text style={styles.subHeading}> download now </Text>
+                      </Pressable>
                     </View>
                   </View>
                 </View>
@@ -116,5 +189,18 @@ const styles = StyleSheet.create({
     fontSize: FontSize.md,
     color: COLORS.BLACK,
     fontWeight: '600',
+  },
+  button: {
+    width: '70%',
+    padding: 10,
+    backgroundColor: '#00BFFF',
+    margin: 10,
+    height: 50,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 5,
   },
 });
